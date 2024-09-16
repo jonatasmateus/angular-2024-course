72c60ed525fd6976f528e7aee2296624ffcf337f
Jonatas <jonatasmateuscontato@gmail.com>
2024-09-16 08:02:41 -0300

    Output function

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, output, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -11,7 +11,10 @@ export class UserComponent {
   @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
-  @Output() select = new EventEmitter();
+  // @Output() select = new EventEmitter();
+
+  // output function is the same as output decorator, and doesn't create any kind of signal
+  select = output<string>();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
