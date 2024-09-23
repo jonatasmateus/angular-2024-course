fe8edb5d48f1c371f979516e4dd294a188034710
Jonatas <jonatasmateuscontato@gmail.com>
2024-09-23 20:25:01 -0300

    More components to the Demo app

--- 01-starting-project/src/app/tasks/tasks.component.ts
+++ 01-starting-project/src/app/tasks/tasks.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  template: `<h1>{{ username }}</h1>`,
+  imports: [TaskComponent],
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-    @Input({ required: true }) username!: string;
+  @Input({ required: true }) username!: string;
 }
