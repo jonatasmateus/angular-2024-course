487a437e0861fe004b9dc7259378de8ff4a58600
Jonatas <jonatasmateuscontato@gmail.com>
2024-10-15 15:24:34 -0300

    Calculate the annual investment data

--- 02-essentials-practice/src/app/user-input/user-input.component.ts
+++ 02-essentials-practice/src/app/user-input/user-input.component.ts
@@ -1,25 +1,32 @@
 import { Component } from '@angular/core';
-import { FormsModule }  from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
   imports: [FormsModule],
   templateUrl: './user-input.component.html',
-  styleUrl: './user-input.component.css'
+  styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
   initialInvestment = '0'; // in $
-  annualInvestiment = '0'; // in $
+  annualInvestment = '0'; // in $
   expectedReturn = '5'; // in %
   duration = '10'; // in month?
-  
+
   onSubmit() {
     console.log(
-      'initialInvestment: ' + this.initialInvestment + '\n' + 
-      'annualInvestment: ' + this.annualInvestiment + '\n' + 
-      'expectedReturn: ' + this.expectedReturn + '\n' + 
-      'duration: ' + this.duration
+      'initialInvestment: ' +
+        this.initialInvestment +
+        '\n' +
+        'annualInvestment: ' +
+        this.annualInvestment +
+        '\n' +
+        'expectedReturn: ' +
+        this.expectedReturn +
+        '\n' +
+        'duration: ' +
+        this.duration
     );
   }
 }
