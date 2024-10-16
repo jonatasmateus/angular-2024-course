9570c797670f0f0dbe37105940f707233967964b
Jonatas <jonatasmateuscontato@gmail.com>
2024-10-16 10:47:04 -0300

    Format output with a currency pipe

--- 02-essentials-practice/src/app/investment-results/investment-results.component.ts
+++ 02-essentials-practice/src/app/investment-results/investment-results.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 
 import type { InvestmentResults } from './investment-results.model';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-investment-results',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css'
 })
