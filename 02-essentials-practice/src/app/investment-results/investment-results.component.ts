168d9089179f2bae7f6b27d42a8dbaaef1b15101
Jonatas <jonatasmateuscontato@gmail.com>
2024-10-17 16:04:22 -0300

    Use service for cross-component communication

--- 02-essentials-practice/src/app/investment-results/investment-results.component.ts
+++ 02-essentials-practice/src/app/investment-results/investment-results.component.ts
@@ -1,15 +1,19 @@
-import { Component, input, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
-import type { InvestmentResults } from './investment-results.model';
 import { CurrencyPipe } from '@angular/common';
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-investment-results',
   standalone: true,
   imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
-  styleUrl: './investment-results.component.css'
+  styleUrl: './investment-results.component.css',
 })
 export class InvestmentResultsComponent {
-  results = input<InvestmentResults[]>();
+  constructor(private investmentService: InvestmentService) {}
+
+  get results() {
+    return this.investmentService.resultsData();
+  }
 }
