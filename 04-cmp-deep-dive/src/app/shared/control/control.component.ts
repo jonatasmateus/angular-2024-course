48cc7358219144edd16330aadc53c81ae6d6e667
Jonatas <jonatasmateuscontato@gmail.com>
2024-10-20 16:35:31 -0300

    Access host elements programmatically

--- 04-cmp-deep-dive/src/app/shared/control/control.component.ts
+++ 04-cmp-deep-dive/src/app/shared/control/control.component.ts
@@ -1,4 +1,12 @@
-import { Component, HostBinding, HostListener, input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  inject,
+  input,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -10,8 +18,8 @@ import { Component, HostBinding, HostListener, input, ViewEncapsulation } from '
   host: {
     // These are the recommeded way (HostBinding & HostListener) which Angular team give to us.
     class: 'control',
-    '(click)': 'onClick()'
-  }
+    '(click)': 'onClick()',
+  },
 })
 export class ControlComponent {
   // @HostBinding('class') className = 'control';
@@ -19,8 +27,9 @@ export class ControlComponent {
   //   console.log('clicked!');
   // }
   label = input.required<string>();
+  private el = inject(ElementRef);
 
   onClick() {
-    console.log('clicked!');
+    console.log(this.el);
   }
 }
