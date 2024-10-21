f769978d1fcf7597a8ff286635854142b5f4c677
Jonatas <jonatasmateuscontato@gmail.com>
2024-10-21 10:52:41 -0300

    ViewChild vs ContentChild

--- 04-cmp-deep-dive/src/app/shared/control/control.component.ts
+++ 04-cmp-deep-dive/src/app/shared/control/control.component.ts
@@ -1,5 +1,7 @@
 import {
   Component,
+  contentChild,
+  ContentChild,
   ElementRef,
   HostBinding,
   HostListener,
@@ -28,8 +30,13 @@ export class ControlComponent {
   // }
   label = input.required<string>();
   private el = inject(ElementRef);
+  // @ContentChild('input') private control?: ElementRef<
+  //   HTMLInputElement | HTMLTextAreaElement
+  // >;
+  private control = contentChild<ElementRef<HTMLInputElement | HTMLTextAreaElement>>('input');
 
   onClick() {
     console.log(this.el);
+    console.log(this.control());
   }
 }
