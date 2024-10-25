d99b6b48bcee3f7976f7bad59e589dae09ff3f7b
Jonatas <jonatasmateuscontato@gmail.com>
2024-10-25 16:13:44 -0300

    Use the alternative dependency injection syntax

--- 07-services-deep-dive/src/app/tasks/tasks.service.ts
+++ 07-services-deep-dive/src/app/tasks/tasks.service.ts
@@ -1,18 +1,19 @@
-import { Injectable, signal } from "@angular/core";
-import { Task } from "./task.model";
+import { Injectable, signal } from '@angular/core';
+import { Task } from './task.model';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class TasksService {
-    tasks = signal<Task[]>([]);
+  private tasks = signal<Task[]>([]);
+  allTasks = this.tasks.asReadonly(); // to garantee that the components will only get the data, instead of manipulate.
 
-    addTask(taskData: { title: string; description: string }) {
-        const newTask: Task = {
-            ...taskData,
-            id: Math.random().toString(),
-            status: 'OPEN'
-        }
-        this.tasks.update((oldTasks) => [...oldTasks, newTask]);
-    }
+  addTask(taskData: { title: string; description: string }) {
+    const newTask: Task = {
+      ...taskData,
+      id: Math.random().toString(),
+      status: 'OPEN',
+    };
+    this.tasks.update((oldTasks) => [...oldTasks, newTask]);
+  }
 }
