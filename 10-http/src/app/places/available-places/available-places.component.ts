3df5b0b3d6151271cd239b6c8b802ba7ab03c4bf
Jonatas <jonatasmateuscontato@gmail.com>
2024-10-29 16:44:17 -0300

    Transform & use response data

--- 10-http/src/app/places/available-places/available-places.component.ts
+++ 10-http/src/app/places/available-places/available-places.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
@@ -18,13 +19,13 @@ export class AvailablePlacesComponent implements OnInit {
 
   ngOnInit() {
     this.httpClient
-      .get<{ places: Place[] }>('http://localhost:3000/places', {
-        observe: 'response'
-      }) // when we add this property, we change the response as *data* to a response as *object*, which can have body, headers, etc.
+      .get<{ places: Place[] }>('http://localhost:3000/places')
+      .pipe(
+        map((resData) => resData.places)
+      )
       .subscribe({
-        next: (response) => {
-          console.log(response);
-          console.log(response.body?.places);
+        next: (places) => {
+          this.places.set(places);
         },
       });
   }
