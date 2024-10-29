37e056aa346b166b9c247fd4f552d1fd97d09fa6
Jonatas <jonatasmateuscontato@gmail.com>
2024-10-29 17:46:16 -0300

    More data fetching & some code duplication

--- 10-http/src/app/places/user-places/user-places.component.ts
+++ 10-http/src/app/places/user-places/user-places.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { map, catchError, throwError } from 'rxjs';
+import { Place } from '../place.model';
+import { HttpClient } from '@angular/common/http';
 
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
@@ -11,4 +14,38 @@ import { PlacesComponent } from '../places.component';
   imports: [PlacesContainerComponent, PlacesComponent],
 })
 export class UserPlacesComponent {
+  places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
+  error = signal('');
+
+  private httpClient = inject(HttpClient);
+
+  ngOnInit() {
+    this.isFetching.set(true);
+    this.httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/user-places')
+      .pipe(
+        map((resData) => resData.places),
+        catchError((err) => {
+          console.log(err);
+          return throwError(
+            () =>
+              new Error(
+                'Something went wrong fetching the available places. Please try again later.'
+              )
+          )}
+        ) // alt. to error property to the subscribe() method.
+      )
+      .subscribe({
+        next: (places) => {
+          this.places.set(places);
+        },
+        error: (err: Error) => {
+          this.error.set(err.message);
+        },
+        complete: () => {
+          this.isFetching.set(false);
+        },
+      });
+  }
 }
