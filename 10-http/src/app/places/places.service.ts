7a64c2852e06331713892a3631de1a4cf9763782
Jonatas <jonatasmateuscontato@gmail.com>
2024-10-30 22:01:48 -0300

    Improved optimistic updating

--- 10-http/src/app/places/places.service.ts
+++ 10-http/src/app/places/places.service.ts
@@ -30,11 +30,21 @@ export class PlacesService {
   }
 
   addPlaceToUserPlaces(place: Place) {
-    this.userPlaces.update(prevPlaces => [...prevPlaces, place]);
+    const prevPlaces = this.userPlaces();
 
+    if (!prevPlaces.some((prevPlace) => prevPlace.id  === place.id)) {
+      this.userPlaces.set([...prevPlaces, place]);
+    }
+
+    
     return this.httpClient.put('http://localhost:3000/user-places', {
       placeId: place.id,
-    });
+    }).pipe(
+      catchError(err => {
+        this.userPlaces.set(prevPlaces);
+        return throwError(() => new Error('Failed to store selected place.'))
+      })
+    );
   }
 
   removeUserPlace(place: Place) {}
