923a26057b7aa35afe547c495484d2c0486c282e
Jonatas <jonatasmateuscontato@gmail.com>
2024-10-30 23:31:00 -0300

    Introduction to HTTP response interceptors

--- 10-http/src/main.ts
+++ 10-http/src/main.ts
@@ -2,11 +2,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 import {
+  HttpEventType,
   HttpHandlerFn,
   HttpRequest,
   provideHttpClient,
   withInterceptors,
 } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 function loggingInterceptor( // interceptor function is the modern way to intercept HTTP requests in Angular, instead class approach.
   request: HttpRequest<unknown>,
@@ -14,7 +16,17 @@ function loggingInterceptor( // interceptor function is the modern way to interc
 ) {
   console.log('[Outgoing Request]');
   console.log(request);
-  return next(request);
+  return next(request).pipe(
+    tap({
+      next: event => {
+        if (event.type === HttpEventType.Response) {
+          console.log('[Incoming Response]');
+          console.log(event.status);
+          console.log(event.body);
+        }
+      }
+    })
+  );
 }
 
 bootstrapApplication(AppComponent, {
