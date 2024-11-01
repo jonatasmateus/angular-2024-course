64e273d5c64e31ad3bebbc69e0baa514e19be0d5
Jonatas <jonatasmateuscontato@gmail.com>
2024-11-01 10:45:46 -0300

    Validate input with form validation directives

--- 11-forms/src/app/auth/login/login.component.ts
+++ 11-forms/src/app/auth/login/login.component.ts
@@ -10,6 +10,10 @@ import { FormsModule, NgForm } from '@angular/forms'
 })
 export class LoginComponent {
   onSubmit(formData: NgForm) {
+    if (formData.form.invalid) {
+      return; // if the form is empty, the form will not be submmitted.
+    }
+
     const enteredEmail = formData.form.value.email;
     const enteredPassword = formData.form.value.password;
     
