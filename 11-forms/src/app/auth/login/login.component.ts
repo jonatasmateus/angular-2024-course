62f107368e13518314817c45a2e814fe3584aa1a
Jonatas <jonatasmateuscontato@gmail.com>
2024-11-02 21:04:45 -0300

    Reactive forms: getting started

--- 11-forms/src/app/auth/login/login.component.ts
+++ 11-forms/src/app/auth/login/login.component.ts
@@ -1,56 +1,15 @@
-import { afterNextRender, Component, viewChild } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
-import { debounceTime } from 'rxjs';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  private form = viewChild.required<NgForm>('form');
-
-  constructor() {
-    afterNextRender(() => {
-      const savedForm = window.localStorage.getItem('saved-login-form');
-
-      if (savedForm) {
-        const loadedFormData = JSON.parse(savedForm);
-        const savedEmail = loadedFormData.email;
-        setTimeout(() => {
-          this.form().controls['email'].setValue(savedEmail);
-        }, 1);
-        // --- If I would like to update the entire form: ---
-        // this.form().setValue({
-        //   email: savedEmail,
-        //   password: ''
-        // })
-      }
-
-      this.form()
-        .valueChanges?.pipe(debounceTime(500))
-        .subscribe({
-          next: (value) =>
-            window.localStorage.setItem(
-              'saved-login-form',
-              JSON.stringify({ email: value.email })
-            ), // with that, this object will be stored in the local storage with every keystroke.
-        });
-    });
-  }
-
-  onSubmit(formData: NgForm) {
-    if (formData.form.invalid) {
-      return; // if the form is empty, the form will not be submmitted.
-    }
-
-    const enteredEmail = formData.form.value.email;
-    const enteredPassword = formData.form.value.password;
-
-    console.log('Email: ' + enteredEmail, ', password: ' + enteredPassword);
-
-    formData.reset();
-  }
+  form = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  });
 }
