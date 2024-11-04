916ab8d3351a2d121349a6044cf45aa170312007
Jonatas <jonatasmateuscontato@gmail.com>
2024-11-04 14:46:58 -0300

    Extract query parameters via observables

--- 12-routing/src/app/tasks/tasks.component.ts
+++ 12-routing/src/app/tasks/tasks.component.ts
@@ -1,8 +1,8 @@
-import { Component, computed, inject, input } from '@angular/core';
+import { Component, computed, inject, input, OnInit } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { TasksService } from './tasks.service';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tasks',
@@ -11,11 +11,21 @@ import { RouterLink } from '@angular/router';
   styleUrl: './tasks.component.css',
   imports: [TaskComponent, RouterLink],
 })
-export class TasksComponent {
+export class TasksComponent implements OnInit {
   userId = input.required<string>();
-  order = input<'asc' | 'desc'>();
+  // order = input<'asc' | 'desc'>();
+  order?: 'asc' | 'desc';
+
   private tasksService = inject(TasksService);
+  private activatedRoute = inject(ActivatedRoute);
+
   userTasks = computed(() =>
     this.tasksService.allTasks().filter((task) => task.userId === this.userId())
   );
+
+  ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe({
+      next: (params) => (this.order = params['order']),
+    });
+  }
 }
