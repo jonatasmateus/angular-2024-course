562d7056a848fdb029053944bc54b14b976a1a0f
Jonatas <jonatasmateuscontato@gmail.com>
2024-11-04 08:51:58 -0300

    Access parent route data from inside nested routes

--- 12-routing/src/app/app.config.ts
+++ 12-routing/src/app/app.config.ts
@@ -1,8 +1,20 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import {
+  provideRouter,
+  withComponentInputBinding,
+  withRouterConfig,
+} from '@angular/router';
 
 import { routes } from './app.route';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withComponentInputBinding())],
+  providers: [
+    provideRouter(
+      routes,
+      withComponentInputBinding(), // this ensure that components can extract path variables according to the declared name. E.g.: '/:userId' → `@Input() userId ...;`
+      withRouterConfig({
+        paramsInheritanceStrategy: 'always', // this ensure that children paths have access to parent path, like path variables. E.g.: '/:userId'
+      })
+    ),
+  ],
 };
