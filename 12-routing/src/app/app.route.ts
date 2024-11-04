b863f633233ce84635f89273a5888a42de2bb544
Jonatas <jonatasmateuscontato@gmail.com>
2024-11-04 14:12:51 -0300

    Split route definitions across multiple files

--- 12-routing/src/app/app.route.ts
+++ 12-routing/src/app/app.route.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
 
+import { routes as userRoutes } from './users/users.routes';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
-import { TasksComponent } from './tasks/tasks.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
@@ -14,21 +13,7 @@ export const routes: Routes = [
   {
     path: 'users/:userId', // <your-domain>/users/<uid>
     component: UserTasksComponent,
-    children: [
-      {
-        path: '', 
-        redirectTo: 'tasks',
-        pathMatch: 'full'
-      },
-      {
-        path: 'tasks', // <your-domain>/users/<uid>/tasks
-        component: TasksComponent,
-      },
-      {
-        path: 'tasks/new', // <your-domain>/users/<uid>/tasks/new
-        component: NewTaskComponent,
-      },
-    ],
+    children: userRoutes
   },
   {
     path: '**', // 'catch all routes'; this route is activated if no other route is met.
