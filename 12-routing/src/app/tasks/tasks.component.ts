bdca0a931721b61d610c69f313b5f4ad9b74b3c8
Jonatas <jonatasmateuscontato@gmail.com>
2024-11-04 23:01:13 -0300

    Use query parameters for data manipulation

--- 12-routing/src/app/tasks/tasks.component.ts
+++ 12-routing/src/app/tasks/tasks.component.ts
@@ -1,4 +1,11 @@
-import { Component, computed, inject, input, OnInit } from '@angular/core';
+import {
+  Component,
+  computed,
+  inject,
+  input,
+  OnInit,
+  signal,
+} from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { TasksService } from './tasks.service';
@@ -14,18 +21,27 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
 export class TasksComponent implements OnInit {
   userId = input.required<string>();
   // order = input<'asc' | 'desc'>();
-  order?: 'asc' | 'desc';
+  order = signal<'asc' | 'desc'>('desc');
 
   private tasksService = inject(TasksService);
   private activatedRoute = inject(ActivatedRoute);
 
   userTasks = computed(() =>
-    this.tasksService.allTasks().filter((task) => task.userId === this.userId())
+    this.tasksService
+      .allTasks()
+      .filter((task) => task.userId === this.userId())
+      .sort((a, b) => {
+        if (this.order() === 'desc') {
+          return a.id > b.id ? -1 : 1;
+        } else {
+          return a.id > b.id ? 1 : -1;
+        }
+      })
   );
 
   ngOnInit(): void {
     this.activatedRoute.queryParams.subscribe({
-      next: (params) => (this.order = params['order']),
+      next: (params) => this.order.set(params['order']),
     });
   }
 }
