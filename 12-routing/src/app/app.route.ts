edbca668488aeca37bcdf27fa1f77f711736f921
Jonatas <jonatasmateuscontato@gmail.com>
2024-11-05 00:37:43 -0300

    Introdution to route guards

--- 12-routing/src/app/app.route.ts
+++ 12-routing/src/app/app.route.ts
@@ -1,9 +1,15 @@
-import { Routes } from '@angular/router';
+import { CanMatchFn, RedirectCommand, Router, Routes } from '@angular/router';
 
 import { routes as userRoutes } from './users/users.routes';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { resolveTitle, resolveUserName, UserTasksComponent } from './users/user-tasks/user-tasks.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { inject } from '@angular/core';
+
+const dummyCanMacth: CanMatchFn = (route, segments) => {
+  const router = inject(Router);
+  return Math.random() < 0.5 ? true : new RedirectCommand(router.parseUrl('/unauthorized'));
+};
 
 export const routes: Routes = [
   {
@@ -14,6 +20,7 @@ export const routes: Routes = [
   {
     path: 'users/:userId', // <your-domain>/users/<uid>
     component: UserTasksComponent,
+    canMatch: [dummyCanMacth],
     children: userRoutes,
     data: {
       message: 'Hello!', // with this, it's possible set this property in components as an input binding, through withComponentInputBinding() called in app.config.ts
